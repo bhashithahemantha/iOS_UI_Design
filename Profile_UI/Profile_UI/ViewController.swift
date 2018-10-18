e6e5cb7f4c30bc01052b9021a4054e2d11b639e1
Bhashitha Hemantha <bhashithahemantha@gmail.com>
2018-10-18 12:08:14 +0530

    initial xcode project for profile UI

---
+++ Profile_UI/Profile_UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Profile_UI
+//
+//  Created by भाषि on 10/18/18.
+//  Copyright © 2018 bhashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
